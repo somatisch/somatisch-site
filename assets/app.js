a71906d07db7ea5b9712e96481906855da0fbeca
Ken Hawkins <khawkins@ebi.ac.uk>
2017-03-25 13:21:38 +0000

    Add forked sharer tool

--- assets/app.js
+++ assets/app.js
@@ -1,1 +1,18 @@
-// Nothing yet
+$('p').flowtype({
+  minFont : 14,
+  maxFont : 26
+});
+$('li, dd, dt').flowtype({
+  minFont : 14,
+  maxFont : 23
+});
+$('h1').flowtype({
+  fontRatio : 10,
+  minFont : 30,
+  maxFont : 70
+});
+
+
+
+// https://github.com/xdamman/selection-sharer
+$('p').selectionSharer();
