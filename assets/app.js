a2428200cfe0f1552b6daf6a5cefa8d50f3935f9
khawkins98 <khawkins98@gmail.com>
2017-04-10 11:15:25 +0100

    Swap to tufte styles

--- assets/app.js
+++ assets/app.js
@@ -1,16 +1,16 @@
-$('p').flowtype({
-  minFont : 14,
-  maxFont : 26
-});
-$('li, dd, dt').flowtype({
-  minFont : 14,
-  maxFont : 23
-});
-$('h1').flowtype({
-  fontRatio : 10,
-  minFont : 30,
-  maxFont : 70
-});
+// $('p').flowtype({
+//   minFont : 14,
+//   maxFont : 26
+// });
+// $('li, dd, dt').flowtype({
+//   minFont : 14,
+//   maxFont : 23
+// });
+// $('h1').flowtype({
+//   fontRatio : 10,
+//   minFont : 30,
+//   maxFont : 70
+// });
 
 
 
